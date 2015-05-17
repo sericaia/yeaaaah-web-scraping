ce212e88fde862e80b2e9bfa49e19eb8fbf4915a
Nelson Neves <nelson.s.neves@gmail.com>
2015-05-17 19:51:41 +0100

    completed webscrapping for yeaaaah

--- index.js
+++ index.js
@@ -8,31 +8,28 @@ var jsonData = [];
 request(url, function (error, response, body) {
 	if (!error) {
 		var $ = cheerio.load(body),
-		links = $("a");
+		concerts = $('table[class=concerts]').find('tr');
 
 
-		links.each(function (i, link) {
-			// get the href attribute of each link
-			var url = $(link).attr("href"),				
-				name;
-
-			if(typeof link.children[0].data != undefined){
-				name = link.children[0].data;
-			}
+		concerts.each(function (i, concert) {
+			// get the href attribute of each concert
+			var name = $(this).find($('.artists')).text().trim();
+			var url = $(this).find($('.artists > a')).attr('href');
+			var date = $(this).parent().prev().text();
+			var cityvenue = $(this).find($('.city_venue')).text().trim();
 
 			var contentData = {
 				url: url,
-				name: name
+				name: name,
+				date: date,
+				city_venue: cityvenue
 			}
 			jsonData.push(contentData);
 
 		});
 
 		console.log(jsonData);
-		console.log("TOTAL: " + links.length)
-		
-
-		console.log("website found!");
+		console.log("TOTAL: " + jsonData.length)
 	} else {
 		console.log("We’ve encountered an error: " + error);
 	}
